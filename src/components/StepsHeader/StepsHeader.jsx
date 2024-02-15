1b17ba18b79fc5ad12343a8a10df2c60cf3cdf37
Facundo Chavez <101282065+facukun@users.noreply.github.com>
2024-02-15 02:58:31 -0300

    v0.0.2

---
+++ src/components/StepsHeader/StepsHeader.jsx
@@ -0,0 +1,13 @@
+import styles from "./StepsHeader.module.scss";
+
+const StepsHeader = () => {
+  
+  //// COMPONENT
+  return (
+    <div className={styles.steps_header}>
+      
+    </div>
+  );
+};
+
+export default StepsHeader;
---
+++ src/components/StepsSlider/StepsSlider.jsx
@@ -0,0 +1,13 @@
+import styles from "./StepsSlider.module.scss";
+
+const StepsSlider = () => {
+  
+  //// COMPONENT
+  return (
+    <div className={styles.steps_slider}>
+      
+    </div>
+  );
+};
+
+export default StepsSlider;
