959ccd6ab324e12733836c010e17f75c64d9d776
Facundo Chavez <101282065+facukun@users.noreply.github.com>
2024-02-26 23:06:10 -0300

    v1.1.8

--- src/context/tour.context.js
+++ src/context/tour.context.js
@@ -61,14 +61,14 @@ const TourProvider = ({ children }) => {
           repiten esos CTA y de otra manera seríamos muy redundantes.
           <br />
           Los links siempre deben ser visibles a menos que no quepan en el
-          viewport y sólo en ese caso se deben resumir en un sidebar. Sería muy
+          viewport y entonces deben ser mudados a un sidebar. Sería muy
           importante tratar de unificar el diseño de todas estas páginas
-          (Academia, Nosotros, Preguntas Frecuentesetc, incluso Políticas de
-          Privacidad) con en diseño y estructura del Inicio (por lo menos mismo
-          Header y mismo Footer), y que no se linkee a aplicaciones de terceros
-          como Notion. Esto va a permitir dar la impresión de un producto más
-          homogéneo y sólido, lo que se traduce en mayor confianza para crearse
-          una cuenta y pagar por el servicio. <br />
+          (Academia, Nosotros, Preguntas Frecuentes, incluso Políticas de
+          Privacidad y Aviso de privacidad) con en diseño y estructura del
+          Inicio (al menos mismo Header y mismo Footer), y que no se linkee a
+          aplicaciones de terceros como Notion. Esto va a permitir presentar al
+          servicio como algo más homogéneo y sólido, lo que se traduce en mayor
+          confianza para crearse una cuenta y pagar por el servicio. <br />
           Las páginas que tengan mucho copywrite y requieran actualizarse
           continuamente (como Preguntas Frecuentes) se pueden desarrollar con la
           misma tecnología que el Inicio y que los copywriters manejen su
@@ -84,10 +84,10 @@ const TourProvider = ({ children }) => {
           Existen varias razones por las que no aplicaría en el Hero (ni en las
           páginas en general) los mismos diseños con personajes propios de las
           publicidades de Filadd. La más importante es la necesidad de una
-          vigencia más alta que la vigencia de las publicidades. Respecto a la
-          altura del Hero me parece excelente la idea actual de que comprenda
-          una altura reducida del viewport para dar de entrada una utilidad
-          (sección siguiente de búsqueda).
+          vigencia más alta que la vigencia de las publicidades. Respecto al
+          Hero me parece excelente la idea actual de que comprenda una altura
+          reducida del viewport para dar de entrada una utilidad (sección
+          siguiente de búsqueda).
         </p>
       ),
       target: () => ref3.current,
@@ -97,7 +97,7 @@ const TourProvider = ({ children }) => {
       description: (
         <p>
           Tener un contador de pasos da más dinamismo y claridad a la búsqueda.
-          Siendo esta la sección estrella (pues ya le está dando una
+          Siendo esta la &quot;sección estrella&quot; (pues ya le está dando una
           funcionalidad a los nuevos usuarios), hay que darle un poco de mimo.
         </p>
       ),
@@ -109,7 +109,7 @@ const TourProvider = ({ children }) => {
         <p>
           Lo más intuitivo es dar un ejemplo de búsqueda de forma directa en el
           placeholder. Me parece lo mejor que el ejemplo sea una universidad del
-          interior, para dejar en claro lo profundo que es el rango de búsqueda.
+          interior, para dejar en claro lo profunda que puede ser la búsqueda.
         </p>
       ),
       target: () => ref5.current,
@@ -124,9 +124,10 @@ const TourProvider = ({ children }) => {
           un orden alfabético sino ponderado en la lista de universidades (las
           más importantes o más buscadas irían primero). También se puede tener
           en cuenta la ubicación del usuario (posterior a solicitar permiso)
-          para esta ponderación. Es importante también reducir la cantidad de
-          items que pueden verse de primera mano, aplicando el botón de “Ver
-          todo”.
+          para esta ponderación.
+          <br />
+          Es importante también reducir la cantidad de items que pueden verse de
+          primera mano, aplicando el botón de “Ver todo” para ampliar la lista.
         </p>
       ),
       target: () => ref6.current,
@@ -137,7 +138,7 @@ const TourProvider = ({ children }) => {
         <p>
           A esta información la traje desde la página actual &quot;Sobre
           nosotros&quot;. Me pareció importante que esté aquí como parte de la
-          narrativa de la landing.
+          narrativa.
         </p>
       ),
       target: () => ref7.current,
@@ -149,7 +150,7 @@ const TourProvider = ({ children }) => {
           Para generar esta sección reuní la información de varias páginas
           actuales, incluso algunas a las que no se puede acceder sin haber
           creado una cuenta. Es muy importante que quede en claro, de la forma
-          más íntegra posible, cómo funciona la aplicación en la página de
+          más íntegra posible, cómo funciona la aplicación en esta página de
           Inicio.
         </p>
       ),
@@ -163,6 +164,9 @@ const TourProvider = ({ children }) => {
           en la landing y esta es la sección ideal para eso. Se puede hacer una
           producción propia o comprar una serie de fotografías coherentes entre
           sí que parezcan más naturales.
+          <br />
+          En la versión móvil las imágenes directamente no aparecen, es una
+          práctica común para mejorar el UX.
         </p>
       ),
       target: () => ref9.current,
@@ -172,9 +176,9 @@ const TourProvider = ({ children }) => {
       description: (
         <p>
           Acá la flashié un poco, pero me parece importante dejar en claro que
-          hacerse Premium no involucra tener acceso directo a todos los cursos,
-          sino que hay que pagar por cada uno de ellos. Esta es una manera de
-          decirlo sin decirlo, pero se puede optar por otras formas.
+          ser Premium no involucra tener acceso directo a todos los cursos, sino
+          que hay que pagar por cada uno de ellos. Esta es una manera de
+          &quot;decirlo sin decirlo&quot;, pero se puede optar por otras formas.
         </p>
       ),
       target: () => ref10.current,
@@ -200,15 +204,17 @@ const TourProvider = ({ children }) => {
           Toda la estructura de la página está diseñada para dirigir a los
           usuarios hacia esta sección de conversión. La mayoría de las
           conversiones ocurren después de leer testimonios, por lo que este es
-          el lugar estratégico. Notarán que no incluí la sección &quot;Cursos de
-          la Academia&quot; en esta landing, ya que interrumpía la narrativa
-          persuasiva. Además, la siguiente sección, &quot;Filadd
-          Profesional&quot;, ya actúa como un elemento adicional, y
-          teóricamente, la sección de conversión debería ser la última antes de
-          llegar al Footer. Tener tantos elementos adicionales al final podría
-          restarle fuerza a esta sección. No obstante, se pueden realizar
-          pruebas A-B para evaluar cómo funcionaría con la sección de cursos o
-          incluso sin la de &quot;Filadd Profesional&quot;.
+          el lugar estratégico.
+          <br />
+          Teóricamente esta debería ser la última sección antes de que aparezca
+          el Footer, así el usuario no se dispersa y no se pierde su conversión.
+          Sin embargo, se pueden hacer algunos tests A-B para evaluar cómo
+          rinden las secciones &quot;Filadd Profesional&quot; (como bien aparece
+          en este caso), &quot;Top cursos de la Academia&quot; e incluso
+          &quot;Planes disponibles&quot;. Todas estas son secciones
+          &quot;extra&quot; que colocadas antes de la conversión cortan un poco
+          con la narrativa persuasiva, por lo que deberían aparecer siempre
+          después, justo antes del Footer.
         </p>
       ),
       target: () => ref12.current,
@@ -217,9 +223,9 @@ const TourProvider = ({ children }) => {
       title: 'Footer',
       description: (
         <p>
-          Al igual que el Header, el Footer también debe proporcionar un link a
-          las distintas páginas para poder seguir navegando desde aquí sin tener
-          que volver hacia arriba.
+          Al igual que el Header, el Footer debe proporcionar un link a las
+          distintas páginas para poder seguir navegando desde aquí sin tener que
+          volver hacia arriba.
         </p>
       ),
       target: () => ref13.current,
